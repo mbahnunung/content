0cacbb3bbb02c023ecb99531fb7ee6fd7f0938cf
mbahnunung <mbahnunung@users.noreply.github.com>
2026-03-08 15:11:45 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+// Sets the cache name
+const CACHE_NAME = 'rbt-radio-v1.0';
+
+// List of files to be cached
+const urlsToCache = [
+  '/',
+  '/live',
+  '/css/rbtlive.css',
+  '/js/combro.js',
+  'https://cdn.bintangtenggarafm.com/img/f69c7be1aa497aaa87f40d2306c4b3578.png',
+  // Add other resources you want to cache here
+];
+
+// Install Service Worker and add cached files
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercepts requests and serves cached files if available
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - returns response from cache
+        if (response) {
+          return response;
+        }
+        // Not found in cache - network search
+        return fetch(event.request);
+      }
+    )
+  );
+});
