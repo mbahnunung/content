f28367308038cb0d2463e1e1c03bfd628a063673
mbahnunung <mbahnunung@users.noreply.github.com>
2026-03-09 13:37:00 +0700

    Update fluoz.js

--- js/fluoz.js
+++ js/fluoz.js
@@ -137,7 +137,7 @@ var mrpjq = jQuery;
                     window.playlistScrollLoading = !0;
                     var f = document.createElement("script");
                     f.type = "text/javascript";
-                    f.src = MRPUtils.qualifyURL(ma + "https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js");
+                    f.src = MRPUtils.qualifyURL(ma + "https://warningfm.github.io/v3/js/jquery.mCustomScrollbar.concat.min.js");
                     f.onload =
                         f.onreadystatechange = function () {
                             this.readyState && "complete" != this.readyState || (X(a), window.playlistScrollLoading = !1)
